91c8508f53eed6527ab368296606e498c0351d4e
oscar <oscar@dragons.mx>
2018-02-10 12:46:34 -0600

    +A messagingEvent

--- index.js
+++ index.js
@@ -12,12 +12,31 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
-
 app.get('/webhook', function (req, res) {
   if (req.query['hub.verify_token'] === 'reportaBot_says_hello') {
       res.send(req.query['hub.challenge']);
   } else {
       res.send('reportaBot_says_bye');
+  }
+});
+
+app.post('/webhook', function (req, res) {
+  const data = req.body;
+  if (data.object == 'page') {
+    data.entry.forEach(function (pageEntry) {
+      pageEntry.messaging.forEach(function (messagingEvent) {
+        if (messagingEvent.message) {
+          if (messagingEvent.message.attachments) {
+            console.log(messagingEvent)
+          } else {
+            console.log(messagingEvent)
+          }
+        } else if (messagingEvent.postback) {
+          console.log(messagingEvent)
+        }
+      });
+    });
+    res.sendStatus(200);
   }
 });
 
